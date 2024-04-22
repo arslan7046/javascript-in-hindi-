e6087cdd9ec38de6ff7a7eda7a1fc1d7d5e61e7f
Arslan <121473469+arslan7046@users.noreply.github.com>
2024-04-22 04:51:10 +0000

    summery

---
+++ one shot js/Datatypes_summer.js
@@ -0,0 +1,28 @@
+// const id = Symbol("123");
+// const anotherId = Symbol("123");
+// console.log(id === anotherId);
+
+//Stack 
+let myYoutubename = "Hiteshchouderydotcom"
+let anothername  =myYoutubename
+anothername = "chaiaurcode"
+// console.log(anothername);
+// console.log(myYoutubename);
+
+let userOne = {
+    email: "user@google.com",
+        upi:"user@ybl"
+
+        }
+        let userTwo = userOne
+        userTwo:email = "hitesh@google.com"
+
+        // console.log(userOne.email);
+        // console.log(userTwo.email);
+
+        const name = "hitesh"
+        const repoCout= 50
+        console.log(`hello  my name is ${name} and this is my repoCount ${repoCout}`);
+
+
+        
