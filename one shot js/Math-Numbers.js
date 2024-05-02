9933c4f0943721452e4e24f02c087b5ceac3c87d
Arslan <121473469+arslan7046@users.noreply.github.com>
2024-05-02 05:32:04 +0000

    chang

--- one shot js/Math-Numbers.js
+++ one shot js/Math-Numbers.js
@@ -1,5 +1,15 @@
 const score = 400
-const balence = new Number (100)
-console.log(balence
-);
+console.log(score);
+const balance = new Number(100)
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(4));
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString("en-IN"));
+
+
 
