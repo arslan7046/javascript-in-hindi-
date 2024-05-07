c565b009a59545b0f38752b5cd2c9d34248c5d06
Arslan <121473469+arslan7046@users.noreply.github.com>
2024-05-07 05:23:06 +0000

    math

--- one shot js/Math-Numbers.js
+++ one shot js/Math-Numbers.js
@@ -9,29 +9,10 @@ console.log(otherNumber.toPrecision(4));
 
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString("en-IN"));
-
-
-// //<!DOCTYPE html>
-// <html>
-// <body>
-
-// <h2>JavaScript Math Constants</h2>
-
-// <p id="demo"></p>
-
-// <script>
-// document.getElementById("demo").innerHTML = 
-// "<p><b>Math.E:</b> " + Math.E + "</p>" +
-// "<p><b>Math.PI:</b> " + Math.PI + "</p>" +
-// "<p><b>Math.SQRT2:</b> " + Math.SQRT2 + "</p>" +
-// "<p><b>Math.SQRT1_2:</b> " + Math.SQRT1_2 + "</p>" +
-// "<p><b>Math.LN2:</b> " + Math.LN2 + "</p>" +
-// "<p><b>Math.LN10:</b> " + Math.LN10 + "</p>" +
-// "<p><b>Math.LOG2E:</b> " + Math.LOG2E + "</p>" +
-// "<p><b>Math.Log10E:</b> " + Math.LOG10E + "</p>";
-// </script>
-
-// </body>
-// </html>
-//
+//console.log(hundreds.toLocaleString("en-IN"));
+//**********************************Math *//
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
