3aa0a8122960cadeca76d046daf880cdd69a880f
Arslan <121473469+arslan7046@users.noreply.github.com>
2024-05-07 04:29:47 +0000

    new string

--- one shot js/strings.js
+++ one shot js/strings.js
@@ -1,3 +1,8 @@
-const name = "hitesh"
-const repoCout= 50
-console.log(`hello  my name is ${name} and this is my repoCount ${repoCout}`);
+const gameName = "hiteshhc";
+console.log(gameName[0]); // Output: h
+console.log(gameName.__proto__); // Output: [String: '']
+
+console.log(gameName.length); // Output: 8
+console.log(gameName.toUpperCase()); // Output: HITESHHC
+console.log(gameName.charAt(2)); // Output: T
+console.log(gameName.indexOf("h")); // Output: 0
