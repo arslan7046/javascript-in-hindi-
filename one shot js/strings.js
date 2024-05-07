fbe880b2443f16bc2f2a8e2e63a665bed13b3ae0
Arslan <121473469+arslan7046@users.noreply.github.com>
2024-05-07 05:00:40 +0000

    complete

--- one shot js/strings.js
+++ one shot js/strings.js
@@ -1,8 +1,27 @@
-const gameName = "hiteshhc";
-console.log(gameName[0]); // Output: h
-console.log(gameName.__proto__); // Output: [String: '']
+const gameName = "hitesh_hc";
+//console.log(gameName[0]); // Output: h
+//console.log(gameName.__proto__); // Output: [String: '']
 
-console.log(gameName.length); // Output: 8
-console.log(gameName.toUpperCase()); // Output: HITESHHC
+//console.log(gameName.length); // Output: 8
+//console.log(gameName.toUpperCase()); // Output: HITESHHC
 console.log(gameName.charAt(2)); // Output: T
 console.log(gameName.indexOf("h")); // Output: 0
+
+
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
+
+const newStringOne = "   hitesh   ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hiteshchoudhary.com/hitesh%20choudhary";
+console.log(url.replace("%20", "_"));
+
+console.log(url.split("/"));
+
