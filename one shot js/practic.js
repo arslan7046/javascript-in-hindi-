b2c919cd6eb6ae791c54e67740a3019f32c13da9
Arslan <121473469+arslan7046@users.noreply.github.com>
2024-11-01 06:12:35 +0000

    ok

---
+++ one shot js/practic.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+//accountId = 2 
+console.log(accountId);
+
+accountEmail = "hcest@google.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+console.table([accountId,accountEmail, accountPassword, accountCity, accountState])
