9f1fefd062710ff67ae480a7a30abc92423a413c
Arslan <121473469+arslan7046@users.noreply.github.com>
2024-11-13 07:19:29 +0000

    converstions

--- one shot js/practic.js
+++ one shot js/practic.js
@@ -10,3 +10,12 @@ accountEmail = "hcest@google.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
 console.table([accountId,accountEmail, accountPassword, accountCity, accountState])
+ 
+
+                   ////////////// Converions\\\\\\\\\\\\\
+                   let score = "30"
+console.log(typeof (score));
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof score);
